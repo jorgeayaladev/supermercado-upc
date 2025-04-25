fde40f26c6bd8ba23c614e18f13e7973d323848b
Jorge Ayala <jayala42000@gmail.com>
2025-04-25 16:22:29 -0500

    Excelente!

--- SupermercadoUPC/SupermercadoUPC.cpp
+++ SupermercadoUPC/SupermercadoUPC.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     cout << "A empezar con el proyecto!!" << endl;
     cout << "Si lees esto es porque ya aprendi a enviar :D"<<endl;
+    cout << "Muy bien hecho!" << endl;
 
     return 0;
 }
